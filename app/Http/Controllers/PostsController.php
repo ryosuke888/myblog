7a1bef1de603947e0f4534d99c8f9941bd70b080
ryosuke888 <rysuke8sr@gmail.com>
2020-07-05 18:09:47 +0900

    myblogを作成しましta

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \App\Post;
+use App\Http\Requests\PostRequest;
+class PostsController extends Controller
+{
+    public function index() {
+        // $posts = Post::all();
+        // $posts = Post::orderBy('created_at', 'desc')->get();
+        $posts = Post::latest()->get();
+        // $posts = [];
+        // dd($posts->toArray());
+        // return view('posts.index', ['posts' => $posts]);
+        return view('posts.index')->with('posts', $posts);
+    }
+    public function show(Post $post) {
+        // $post = Post::findOrfail($id);
+        return view('posts.show')->with('post', $post);
+    }
+    public function create(Post $post) {
+        // $post = Post::findOrfail($id);
+        return view('posts.create');
+    }
+    public function store(PostRequest $request) {
+        $post = new Post();
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+        return redirect('/');
+    }
+    public function edit(Post $post) {
+        // $post = Post::findOrfail($id);
+        return view('posts.edit')->with('post', $post);
+    }
+    public function update(RequPostRequestest $request, Post $post) {       
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+        return redirect('/');
+    }
+    public function destroy(Post $post) {
+        $post->delete();
+        return redirect('/');
+    }
+}
